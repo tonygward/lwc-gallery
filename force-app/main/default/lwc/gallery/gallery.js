aa264812241464db569d9eadba86ceb8a0666ca3
Tony Ward <tonygward@googlemail.com>
2024-09-06 15:07:28 +0100

    Added Gallery

--- force-app/main/default/lwc/gallery/gallery.js
+++ force-app/main/default/lwc/gallery/gallery.js
@@ -4,23 +4,28 @@ export default class Gallery extends LightningElement {
     components = [
         {
             name: 'lightning:accordion',
-            description: 'A simple accordion'
+            description: 'A simple accordion',
+            image: 'genericIcon.png'
         },
         {
             name: 'lightning:alert',
-            description: 'create an alert'
+            description: 'create an alert',
+            image: 'genericIcon.png'
         },
         {
             name: 'lightning:avatar',
-            description: 'create an avatar'
+            description: 'create an avatar',
+            image: 'avatar.png'
         },
         {
             name: 'lightning:badge',
-            description: 'create a badge'
+            description: 'create a badge',
+            image: 'genericIcon.png'
         },
         {
             name: 'lightning:breadcrumb',
-            description: 'create a breadcrumb'
+            description: 'create a breadcrumb',
+            image: 'genericIcon.png'
         }
     ];
 }
--- force-app/main/default/lwc/galleryComponent/galleryComponent.js
+++ force-app/main/default/lwc/galleryComponent/galleryComponent.js
@@ -1,10 +1,16 @@
 import { LightningElement, api } from 'lwc';
+import componentIcons from "@salesforce/resourceUrl/galleryComponentIcons"
 
 export default class GalleryComponent extends LightningElement {
     @api name;
     @api description;
+    @api image;
 
     get detailUrl() {
         return '/lightning/cmp/c__galleryComponentDetail';
+    }
+
+    get imageUrl() {
+        return componentIcons + '/' + this.image;
     }
 }
